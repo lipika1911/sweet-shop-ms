40d3f1f968d71fd880cf758ae7606f24dc6af2c9
lipika1911 <lipikaarya1723@gmail.com>
2025-12-14 15:16:12 +0530

    chore: connect MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import "dotenv/config";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+  });
+});
