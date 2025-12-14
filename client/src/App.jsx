4341f1c1c9982e93e7847dbe0b04010cd5645206
lipika1911 <lipikaarya1723@gmail.com>
2025-12-14 21:03:40 +0530

    chore: initial setup with Reactjs + tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='m-4 flex flex-col items-center justify-center'>
+      <h1 className='text-4xl'>SWEET SHOP</h1>
+    </div>
+  )
+}
+
+export default App
